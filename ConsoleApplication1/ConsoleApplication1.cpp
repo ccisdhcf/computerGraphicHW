dfdfa52e41bd50aab10a073819d9ea39e8701947
DESKTOP-Q6BBGDK\wmlab <acd789456123@g.ncu.edu.tw>
2021-10-13 22:36:56 +0800

    line test fail1

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -3,12 +3,75 @@
 #include <GL/glut.h>
 #include <GL/gl.h>
 #include<iostream>
+#include <list>
+#include "ConsoleApplication1.h"
+#include<math.h>
 using namespace std;
 
+
+int forLoopOptimize = 1;
+int mode = 0;
+class point
+{
+private:
+	int x;
+	int y;
+public:
+	point(int _x, int _y) {
+		x = _x;
+		y = _y;
+	};
+	int getX() {
+		return x;
+	};
+	int getY() {
+		return y;
+	}
+	void setX(int new_x) {
+		x = new_x;
+	}
+	void setY(int new_y) {
+		y = new_y;
+	}
+};
+list<point> pointList;
+//int rounding(double num, int index = 0)
+//{
+//	bool isNegative = false; // whether is negative number or not
+//
+//	if (num < 0) // if this number is negative, then convert to positive number
+//	{
+//		isNegative = true;
+//		num = -num;
+//	}
+//
+//	if (index >= 0)
+//	{
+//		int multiplier;
+//		multiplier = pow(10, index);
+//		num = (int)(num * multiplier + 0.5) / (multiplier * 1.0);
+//	}
+//
+//	if (isNegative) // if this number is negative, then convert to negative number
+//	{
+//		num = -num;
+//	}
+//
+//	return (int)num;
+//}
+//point edgeCheck(float _x, float _y) {
+//	int x = rounding(_x);
+//	int y = rounding(_y);
+//	// printf("%d %d %f %f \n", x, y , _x ,_y); //debug
+//	return point(x, y);
+//}
 void display() {
 	glClearColor(0.0, 0.0, 0.0, 0.0);
 	glClear(GL_COLOR_BUFFER_BIT);
 	glFlush();
+}
+void clear() {
+
 }
 
 void drawSquare(int x, int y) {
@@ -17,22 +80,121 @@ void drawSquare(int x, int y) {
 	glBegin(GL_POINTS);
 	glVertex2i(x, glutGet(GLUT_WINDOW_HEIGHT) - y);
 	glEnd();
-	glutSwapBuffers();
+
+}
+void drawCycle(point centre, point cyclePoint) {  //超出邊界(?
+	float x_2 = pow((cyclePoint.getX() - centre.getX()), 2);
+	float y_2 = pow((cyclePoint.getY() - centre.getY()), 2);
+	float radius = pow((x_2 + y_2), 0.5);
+	pointList.push_back(point(centre.getX() + (int)radius, centre.getY()));
+	pointList.push_back(point(centre.getX() - (int)radius, centre.getY()));
+	for (int tempX = centre.getX() - (int)radius; tempX < centre.getX() + (int)radius; tempX+= forLoopOptimize)
+	{
+		int tempY_Abs = pow(radius * radius - (tempX - centre.getX()) * (tempX - centre.getX()),0.5);
+		pointList.push_back(point(tempX, centre.getY()+tempY_Abs));
+		pointList.push_back(point(tempX, centre.getY()-tempY_Abs));
+
+	}
+}
+
+void drawLine(point src,point des) {
+	int dy = des.getY() - src.getY();
+	int dx = des.getX() - src.getX();
+	float d_init = dy + (float)dx / 2;
+	int incE = dy;
+	int incNE = dy+dx;
+	float d = d_init;
+	if ((float) dy / dx <= 1) {
+		if (dx < 0) {   // des.x < src.x then switch
+			point temp = point(des.getX(), des.getY());
+			des.setX(src.getX());
+			des.setY(src.getY());
+			src.setX(temp.getX());
+			src.setY(temp.getY());
+			dy = -dy;
+			dx = -dx;
+			d_init = -d_init;
+			d = -d;
+			incE = -incE;
+			incNE = -incNE;
+			//d?
+
+		}
+
+		int x = src.getX();
+		int y = src.getY();
+		pointList.push_back(point(x, y));
+		while (x<=des.getX())
+		{
+			cout << d << "\n\n";
+			if (d <= 0) {
+				x+=1;
+				cout << x <<" " << y << "\n";
+
+				pointList.push_back(point(x, y));
+
+				d += incE;
+			}
+			else
+			{
+				x+=1;
+				y+=1;
+				cout << x <<" " << y << "\n";
+				pointList.push_back(point(x , y));
+				d += incNE;
+			}
+		}
+	}
+	else  //switch (y,x)
+	{
+
+	}
+	
+	//pointList.push_back(src);
+	//pointList.push_back(des);
+	//if (des.getX() == src.getX()) //斜率無限大
+	//{
+	//	for (int tempY  = min(src.getY(),des.getY())+1;tempY  < max(src.getY(),des.getY()); tempY+= forLoopOptimize)
+	//	{
+	//		pointList.push_back(point(des.getX(), tempY));
+	//	}
+	//}
+	//else  //有斜率or = 0
+	//{
+	//	for (int tempX = min(src.getX() + 1,des.getX()+1); tempX < max(des.getX(),src.getX()); tempX+= forLoopOptimize)
+	//	{
+
+	//		float tempY = (float)src.getY() + (tempX-src.getX())*(des.getY() - src.getY()) / (float)(des.getX() - src.getX());
+	//		point temp=edgeCheck(tempX, tempY);
+	//		pointList.push_back(temp);
+	//	}
+	//}
+
 }
 
 void mouse(int bin, int state, int x, int y) {
-	if (bin == GLUT_LEFT_BUTTON && state == GLUT_DOWN) drawSquare(x, y);
+	point temp=point(x ,y);
+	if (state == GLUT_DOWN) {
+		pointList.push_back(temp);
+	}
+	list<point> ::iterator point;
+	for (point = pointList.begin(); point != pointList.end();point++) {
+		drawSquare(point->getX(), point->getY());
+	}
+	glutSwapBuffers();
+
+	//if (bin == GLUT_LEFT_BUTTON && state == GLUT_DOWN) drawSquare(x, y);
 }
 
 int main(int argc, char** argv) {
 	glutInit(&argc, argv);
-
 	glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE);
 	glutInitWindowSize(500, 500);
 	glutInitWindowPosition(100, 100);
 	gluOrtho2D(0, 500, 0, 500);
 	glutCreateWindow("Your First GLUT  Window!");
-
+	//drawCycle(point(250, 250), point(250,200)); //debug
+	drawLine(point(0, 0), point(250, 50));  //debug
 	glutDisplayFunc(display);
 	glutMouseFunc(mouse);
 	gluOrtho2D(0, 500, 0, 500);
---
+++ ConsoleApplication1/ConsoleApplication1.h
@@ -0,0 +1,3 @@
+#pragma once
+
+void mouse(int bin, int state, int x, int y);
